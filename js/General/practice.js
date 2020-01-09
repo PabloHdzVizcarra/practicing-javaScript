faaa71803ad3b8fbdbeb6baea7c80e8995285f5b
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-01-09 17:41:31 -0600

    se termina el capitulo 1 del libro

--- js/General/practice.js
+++ js/General/practice.js
@@ -1,2 +1,26 @@
 'use strict'
 
+
+console.log(100 + 4 * 100);
+console.log(0 / 0);
+console.log("Esta es la primer linea \ny esta la segunda");
+console.log("un caracter de nueva linea");
+console.log(typeof 3);
+console.log(typeof 'Hola');
+console.log(4 > 3);
+console.log('Z' > 'a');
+console.log(4 < 5 && 8 < 6);
+console.log(!true);
+console.log(10 < 9 ? 'Verdadero' : 'Falso');
+
+console.log(8 * null);
+console.log(5 - '3');
+console.log(null == undefined);
+console.log(null || 'User');
+console.log('Thiago' || 'Yara');
+console.log(!false);
+
+function checkNumberLess(num) {
+    return 10 != num ? 'Correcto':'Falso'
+}
+console.log(checkNumberLess(10));
