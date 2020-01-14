fcbe6c53495e5c55f07cd9ce8a848b1352fe7053
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-01-14 13:44:21 -0600

    se terminan los ejercicios del capitulo 2

--- js/Eloquent Javascript/excersiseChapter2.js
+++ js/Eloquent Javascript/excersiseChapter2.js
@@ -1,31 +1,69 @@
-'use strict'
+"use strict";
 
+// Excersise I
 
-
-let line = '';
+let line = "";
 for (let i = 0; i < 7; i++) {
-    line += '#'
-    console.log(line);
+  line += "#";
+  console.log(line);
 }
 
 // FizzBuzz
 
-for (let i = 1; i <= 100; i++){
+// for (let i = 1; i <= 100; i++){
 
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log('FizzBuzz');
+//     }
+//     else if(i % 3 == 0) {
+//         console.log('Fizz');
+//     }
+//     else if (i % 5 == 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// Excersise II
+
+function chess(num) {
+  let signal = "";
+  for (let i = 0; i < num; i++) {
+    for (let x = 0; x < num; x++) {
+      if ((x + i) % 2 == 0) {
+        signal += " ";
+      } else {
+        signal += "#";
+      }
     }
-    else if(i % 3 == 0) {
-        console.log('Fizz');  
-    }
-    else if (i % 5 == 0) {
-        console.log('Buzz');
+    signal += "\n";
+  }
+
+  return signal;
+}
+console.log(chess(10));
+
+
+
+function happyBirthdayJanuary() {
+  let lucero = 12,
+    mom = 31,
+    daysMoth = 31;
+
+  for (let i = 1; i <= daysMoth; i++) {
+    if (i === lucero) {
+      console.log(`!!!El dia ${lucero} tu esposa Lucero cumple años`);
+    } else if (i === mom) {
+      console.log(`!!!El dia ${mom} tu mama cumple años`);
     } else {
-        console.log(i);
+      console.log(`${i} de Enero nadie cumple años`);
     }
-}
+  }
 
-let date = 8;
-for (let i = 0; i < date; i++) {
-    
+  return "Termino de programa";
 }
+// console.log(happyBirthdayJanuary());
+
+
+//
